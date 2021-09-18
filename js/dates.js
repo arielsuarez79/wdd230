0f1a8b40b3a8356bc3f287ec39bcfecc121c2161
arielsuarez79 <sua21007@byui.edu>
2021-09-17 23:50:04 -0300

    yes

--- js/dates.js
+++ js/dates.js
@@ -1,2 +1,4 @@
 const copyrightyear = document.getElementById("copyrightyear");
 copyrightyear.textContent = new Date().getFullYear();
+
+document.getElementById("lastmodified").innerHTML = new Date(document.lastModified);
