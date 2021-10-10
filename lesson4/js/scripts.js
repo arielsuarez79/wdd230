af54bbb2f3ad413e20804b6dd5bebbecd9444470
arielsuarez79 <sua21007@byui.edu>
2021-10-09 22:40:52 -0300

    lesson4

---
+++ lesson4/js/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
