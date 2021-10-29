732a4e06efc043a8b9c5d4e8a68f4cab899c708e
arielsuarez79 <sua21007@byui.edu>
2021-10-28 23:56:29 -0300

    lesson7

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,13 +1,19 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
     image.removeAttribute('data-src');
   };
 };
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+
+
 
 if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
@@ -17,7 +23,7 @@ if('IntersectionObserver' in window) {
           observer.unobserve(item.target);
         }
       });
-    });
+    }, imgOptions);
     imagesToLoad.forEach((img) => {
       observer.observe(img);
     });
