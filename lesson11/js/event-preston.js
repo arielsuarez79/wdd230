10bcd37a8867d7e1492123547c618605d57fffd5
arielsuarez79 <sua21007@byui.edu>
2021-11-28 02:48:03 -0300

    lesson11

--- lesson11/js/event-preston.js
+++ lesson11/js/event-preston.js
@@ -8,23 +8,15 @@ fetch(infoURL)
     console.table(jsonObject);
     const towns = jsonObject["towns"];
     towns.forEach((town) => {
-      if (
-        town.name == "Preston" ||
-        town.name == "Soda Springs" ||
-        town.name == "Fish Haven"
-      ) {
-      
+      if (town.name == "Preston") {
+        // Create Event Section //
         let sectionEvent = document.createElement("section");
-        let h3Event = document.createElement("h3");
         let divContCardEvent = document.createElement("div");
         let events = town.events;
         let allEvents = document.createElement("div");
 
-
         sectionEvent.setAttribute("class", "town-event");
         divContCardEvent.setAttribute("class", "events");
-
-        h3Event.textContent = town.name;
 
         events.forEach((event) => {
           let eventPar = document.createElement("p");
@@ -33,9 +25,8 @@ fetch(infoURL)
         });
 
         allEvents.setAttribute("class", "events-name");
-        h3Event.setAttribute("class","h3-title");
         divContCardEvent.append(allEvents);
-        sectionEvent.append(h3Event, divContCardEvent);
+        sectionEvent.append(divContCardEvent);
         document.querySelector("div.town-events-info").append(sectionEvent);
       }
     });
