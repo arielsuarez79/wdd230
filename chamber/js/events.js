cc1ba10cac545efba25338870fe11d0fa4b97f10
arielsuarez79 <sua21007@byui.edu>
2021-12-15 16:33:44 -0300

    chamber

--- chamber/js/events.js
+++ chamber/js/events.js
@@ -1,4 +1,4 @@
-const infopath = "../js/tucuman.json";
+const infopath = "js/tucuman.json";
 
 fetch(infopath)
   .then((response) => {
@@ -14,29 +14,29 @@ fetch(infopath)
        // town.name == "Fish Haven"
      
       
-        let sectionEvent = document.createElement("section");
-        let h3Event = document.createElement("h3");
-        let divContCardEvent = document.createElement("div");
-        let events = town.events;
-        let allEvents = document.createElement("div");
+      let sectionEvent = document.createElement("section");
+      let h3Event = document.createElement("h3");
+      let divContCardEvent = document.createElement("div");
+      let events = town.events;
+      let allEvents = document.createElement("div");
 
 
-        sectionEvent.setAttribute("class", "town-event");
-        divContCardEvent.setAttribute("class", "events");
+      sectionEvent.setAttribute("class", "town-event");
+      divContCardEvent.setAttribute("class", "events");
 
-        h3Event.textContent = town.name;
+      h3Event.textContent = town.name;
 
-        events.forEach((event) => {
-          let eventPar = document.createElement("p");
-          eventPar.textContent = event;
-          allEvents.append(eventPar);
-        });
+      events.forEach((event) => {
+        let eventPar = document.createElement("p");
+        eventPar.textContent = event;
+        allEvents.append(eventPar);
+      });
 
-        allEvents.setAttribute("class", "events-name");
-        h3Event.setAttribute("class","h3-title");
-        divContCardEvent.append(allEvents);
-        sectionEvent.append(h3Event, divContCardEvent);
-        document.querySelector("div.town-events-info").append(sectionEvent);
-      
+      allEvents.setAttribute("class", "events-name");
+      h3Event.setAttribute("class","h3-title");
+      divContCardEvent.append(allEvents);
+      sectionEvent.append(h3Event, divContCardEvent);
+      document.querySelector("div.town-events-info").append(sectionEvent);
+    
     });
   });
--- chamber/js/index.js
+++ chamber/js/index.js
@@ -1,29 +1,29 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
-fetch(requestURL)
+//const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const infopath = "js/tucuman.json";
+fetch(infopath)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
     //console.table(jsonObject);  // temporary checking for valid response and data parsing
-        const prophets = jsonObject['towns'];
-        const change = prophets.filter((town) => town.name === 'Preston'|| town.name === 'Soda Springs'|| town.name === 'Fish Haven');
-        change.forEach((x) => {
+        const prophets = jsonObject['directory'];
+        
+        prophets.forEach((x) => {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
-            let motto = document.createElement('h3');
-            let founded = document.createElement('p');
-            let population = document.createElement('p');
+            let addres = document.createElement('p');
+            let phone = document.createElement('p');
+            let website = document.createElement('p');
             let rain = document.createElement('p');
             let img = document.createElement('img');
 
             h2.textContent = x.name;
-            motto.textContent = x.motto;
-            founded.textContent = "Year Founded: "+x.yearFounded;
-            population.textContent = "Population: "+x.currentPopulation;
-            rain.textContent = "Annual Rain Fall: "+x.averageRainfall;
+            addres.textContent = "Addres: " +x.addres;
+            phone.textContent = "Phone: "+x.phone;
+            website.textContent = "Website: "+x.website;
+            
     
-            img.setAttribute('src', '../lesson9/images/' + x.photo);
+            img.setAttribute('src', '../chamber/images/' + x.photo);
             img.setAttribute('alt', x.name + x.lastname);
 
             card.appendChild(h2);
